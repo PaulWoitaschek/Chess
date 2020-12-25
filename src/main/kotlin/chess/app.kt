604e715974d77c9a146d4c0d36db9bccc16ce5e9
Paul Woitaschek <paul@yazio.com>
2020-12-25 14:05:51 +0100

    Draw the board first

--- src/main/kotlin/chess/app.kt
+++ src/main/kotlin/chess/app.kt
@@ -10,16 +10,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.gesture.DragObserver
 import androidx.compose.ui.gesture.dragGestureFilter
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.res.vectorXmlResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toOffset
 import kotlin.math.roundToInt
 
 val game = Game()
@@ -39,22 +40,38 @@ fun main() {
         }) {
           Text("Restart, Play black")
         }
-        game.board.value.forEachIndexed { rowIndex, column ->
-          val evenRow = rowIndex % 2 == 0
-          Row {
+        Box {
+          Column {
+            game.board.value.forEachIndexed { rowIndex, column ->
+              val evenRow = rowIndex % 2 == 0
+              Row {
+                column.forEachIndexed { columnIndex, chessPiece ->
+                  val coordinate = Coordinate(row = rowIndex, column = columnIndex)
+                  val evenColumn = columnIndex % 2 == 0
+                  val blackField = (evenRow && !evenColumn) || (evenColumn && !evenRow)
+                  val color = if (blackField) Color(0xFF616161) else Color(0xffbdbdbd)
+                  val strokeColor = if (coordinate in game.validMoves.value) Color(0xff66bb6a) else Color.Transparent
+                  Box(Modifier.width(itemSize)
+                    .background(color = color)
+                    .height(itemSize)
+                    .border(width = 6.dp, color = strokeColor))
+                }
+              }
+            }
+          }
+          game.board.value.forEachIndexed { rowIndex, column ->
+            val evenRow = rowIndex % 2 == 0
             column.forEachIndexed { columnIndex, chessPiece ->
               val coordinate = Coordinate(row = rowIndex, column = columnIndex)
-              val evenColumn = columnIndex % 2 == 0
-              val blackField = (evenRow && !evenColumn) || (evenColumn && !evenRow)
-              val color = if (blackField) Color(0xFF616161) else Color(0xffbdbdbd)
-              val strokeColor = if (coordinate in game.validMoves.value) Color(0xff66bb6a) else Color.Transparent
-              Box(Modifier.width(itemSize)
-                .background(color = color)
-                .height(itemSize)
-                .border(width = 6.dp, color = strokeColor)) {
-                if (chessPiece != null) {
-                  ChessPieceImage(type = chessPiece, coordinate)
-                }
+              if (chessPiece != null) {
+                ChessPieceImage(type = chessPiece, coordinate, modifier = Modifier
+                  .size(itemSize)
+                  .layout { measurable, constraints ->
+                    val placeable = measurable.measure(constraints)
+                    layout(placeable.width, placeable.height) {
+                      placeable.place(columnIndex * placeable.width, rowIndex * placeable.height)
+                    }
+                  })
               }
             }
           }
@@ -66,7 +83,7 @@ fun main() {
 
 @Suppress("FunctionName")
 @Composable
-fun BoxScope.ChessPieceImage(
+fun ChessPieceImage(
   type: Piece,
   coordinate: Coordinate,
   modifier: Modifier = Modifier
@@ -76,6 +93,11 @@ fun BoxScope.ChessPieceImage(
     override fun onStart(downPosition: Offset) {
       offset.value = downPosition
       game.startMove(coordinate)
+    }
+
+    override fun onCancel() {
+      offset.value = Offset.Zero
+      game.stopMove()
     }
 
     override fun onDrag(dragDistance: Offset): Offset {
@@ -99,12 +121,12 @@ fun BoxScope.ChessPieceImage(
   Image(
     vectorXmlResource(type.path),
     modifier = modifier
-      .fillMaxSize(0.7F)
-      .align(Alignment.Center)
       .dragGestureFilter(dragObserver, canDrag = { true }, startDragImmediately = true)
       .offset {
         val value = offset.value
-        IntOffset(value.x.roundToInt(), value.y.roundToInt())
+        IntOffset(value.x.roundToInt(), value.y.roundToInt()).also {
+          println("itOffset=$it")
+        }
       }
   )
 }
